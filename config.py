cb3e97a0339a51f747cf278591f44d3c2c5ddf39
Roshan Sharma <reprogram46@gmail.com>
2019-09-20 12:05:55 +0530

    feat: Set random unicode secret key
    Use "os.urandom" to generate a random 42 byte key.

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(42)
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
